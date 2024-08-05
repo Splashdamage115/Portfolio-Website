3fef16130129d7c53feba175c19967f26b462879
David Strikaitis <davidstrik282@gmail.com>
2024-08-05 02:38:25 +0100

    temporarily removed canvas

--- src/assets/js/timeLine.js
+++ src/assets/js/timeLine.js
@@ -68,12 +68,13 @@ function loadTimeLineContent(t_jsonFile)
     newItem = createElement('p', {}, "(See Projects on left, and click to enter Project)");
     document.getElementById("clearWrapper").appendChild(newItem);
 
-    newItem = createElement('canvas', {id:"the_canvas"}, "oops! it looks like your browser doesnt support canvas!")
+    // Temporarily removing canvas till ready to work on!
+    /*newItem = createElement('canvas', {id:"the_canvas"}, "oops! it looks like your browser doesnt support canvas!")
     document.getElementById("clearWrapper").appendChild(newItem);
     canvas = document.getElementById("the_canvas");
     context  = canvas.getContext("2d");
 
-    animate = true;
+    animate = true;*/
     
     newItem = createElement('p', {} ,'<br>');
     document.getElementById("clearWrapper").appendChild(newItem);
@@ -82,7 +83,10 @@ function loadTimeLineContent(t_jsonFile)
     newItem.appendChild(createElement('source', {src:"src/assets/imgs/Render.mp4", type:"video/mp4"}))
     document.getElementById("clearWrapper").appendChild(newItem);
 
-    animateMenuItems();
+    // ***********************************************************
+    //            Recomment this in too for the canvas
+    // ***********************************************************
+    //animateMenuItems();
 }
 
 
