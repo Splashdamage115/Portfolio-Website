3bbb85029bd8a1b1d56817494a6b4e3ec9b3f578
DavidStrikaitis <124636547+Splashdamage115@users.noreply.github.com>
2025-03-12 02:43:10 +0100

    Update spanContent.js

--- src/assets/js/spanContent.js
+++ src/assets/js/spanContent.js
@@ -5,7 +5,7 @@ function openGitHub() {
     window.open('https://www.linkedin.com/in/david-strikaitis-59060b2a2/?trk=opento_sprofile_topcard', '_blank');
   }
   function openEmail() {
-    window.open('mailto:davidstrik282@gmail.com', '_blank');
+    window.open('mailto:davidstrikaitis@gmail.com', '_blank');
   }
 
 document.getElementById('home').addEventListener('click', function () { loadGame('HomeScreen'); });
